bc7b8794e12e47a45996203d45189cd085b1650b
msk- <mattkingston@gmail.com>
2019-02-14 22:01:59 +0000

    MOWDEV-236 moved current UI into a component to prepare for tabs

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 
 import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
 import './App.css';
-import { withStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-// TODO: export all components from components directory in an index.js, then import them all here
-// in a single statement
-import SettlementsList from './components/SettlementsList';
-import SettlementWindowInfo from './components/SettlementWindowInfo';
-import FSPSelector from './components/FSPSelector';
 import Login from './components/Login';
+import SettlementsTab from './components/SettlementsTab';
 import { get } from './requests';
 import { getUserInfo, setUserInfo } from './user';
 
@@ -18,36 +10,11 @@ import { getUserInfo, setUserInfo } from './user';
 //       https://reactjs.org/docs/error-boundaries.html
 // TODO: do we need to add styling to all the components? (Or should we get rid of this material
 //       thing?)
-
-const styles = theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing.unit * 2,
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
-});
+// TODO: consider a big "DEVELOPMENT MODE" header when in development mode
 
 function App(props) {
-  const { classes } = props;
-
-  const [selectedFsp, setSelectedFsp] = useState(undefined); // TODO: remove?
-  const [settlementWindowState, setSettlementWindowState] = useState(undefined);
-  const [settlements, setSettlements] = useState(undefined);
   const [user, setUser] = useState(getUserInfo());
   const [fspList, setFspList] = useState(undefined);
-
-  const selectFsp = async (dfspId) => {
-    const [win, settlements] = await Promise.all(([
-      get(`current-window/${dfspId}`),
-      get(`settlements/${dfspId}`)
-    ]));
-    setSettlementWindowState(win);
-    setSettlements(settlements);
-    setSelectedFsp(dfspId);
-  };
 
   const loginSuccessful = result => {
     setUserInfo(result);
@@ -77,41 +44,13 @@ function App(props) {
 
   // TODO: what are the md (and xs, etc.) props on Grid?
   return (
-    <div className={classes.root}>
+      <>
       {user === undefined ?
         <Login loginSuccessful={loginSuccessful} /> :
-        (fspList === undefined ? <></> :
-        <Grid container spacing={24}>
-          <Grid item md={12} />
-          <Grid item md={4}>
-            <Paper className={classes.paper}>
-              <FSPSelector selectFsp={selectFsp} fspList={fspList} />
-            </Paper>
-          </Grid>
-          {settlementWindowState === undefined || settlements === undefined ? <></> :
-          <Grid item md={8}>
-            <Grid container spacing={24}>
-              <Grid item md={12}>
-                <Paper className={classes.paper}>
-                  <SettlementWindowInfo settlementWindow={settlementWindowState.window} positions={settlementWindowState.positions} />
-                </Paper>
-              </Grid>
-              <Grid item md={12}>
-                <Paper className={classes.paper}>
-                  <SettlementsList selectedFsp={selectedFsp} settlements={settlements} fspList={fspList} />
-                </Paper>
-              </Grid>
-            </Grid>
-          </Grid>
-          }
-        </Grid>
+        (fspList === undefined ? <></> : <SettlementsTab fspList={fspList} />
         )}
-    </div>
+      </>
   );
 }
 
-App.propTypes = {
-  classes: PropTypes.object.isRequired
-};
-
-export default withStyles(styles)(App);
+export default App;
