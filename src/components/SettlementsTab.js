497dd822c47b101b462e008c7e673d2606351b8e
msk- <mattkingston@gmail.com>
2019-02-14 22:01:45 +0000

    MOWDEV-236 moved current UI into a component to prepare for tabs

---
+++ src/components/SettlementsTab.js
@@ -0,0 +1,77 @@
+
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+// TODO: export all components from components directory in an index.js, then import them all here
+// in a single statement
+import SettlementsList from './SettlementsList';
+import SettlementWindowInfo from './SettlementWindowInfo';
+import FSPSelector from './FSPSelector';
+import { get } from '../requests';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+});
+
+function SettlementsTab(props) {
+  const { fspList, classes } = props;
+
+  const [settlements, setSettlements] = useState(undefined);
+  const [settlementWindowState, setSettlementWindowState] = useState(undefined);
+  const [selectedFsp, setSelectedFsp] = useState(undefined); // TODO: remove?
+
+  const selectFsp = async (dfspId) => {
+    const [win, settlements] = await Promise.all(([
+      get(`current-window/${dfspId}`),
+      get(`settlements/${dfspId}`)
+    ]));
+    setSettlementWindowState(win);
+    setSettlements(settlements);
+    setSelectedFsp(dfspId);
+  };
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={24}>
+        <Grid item md={12} />
+        <Grid item md={4}>
+          <Paper className={classes.paper}>
+            <FSPSelector selectFsp={selectFsp} fspList={fspList} />
+          </Paper>
+        </Grid>
+        {settlementWindowState === undefined || settlements === undefined ? <></> :
+        <Grid item md={8}>
+          <Grid container spacing={24}>
+            <Grid item md={12}>
+              <Paper className={classes.paper}>
+                <SettlementWindowInfo settlementWindow={settlementWindowState.window} positions={settlementWindowState.positions} />
+              </Paper>
+            </Grid>
+            <Grid item md={12}>
+              <Paper className={classes.paper}>
+                <SettlementsList selectedFsp={selectedFsp} settlements={settlements} fspList={fspList} />
+              </Paper>
+            </Grid>
+          </Grid>
+        </Grid>
+        }
+      </Grid>
+    </div>
+  );
+}
+
+SettlementsTab.propTypes = {
+  classes: PropTypes.object.isRequired,
+  fspList: PropTypes.array.isRequired
+};
+
+export default withStyles(styles)(SettlementsTab);
