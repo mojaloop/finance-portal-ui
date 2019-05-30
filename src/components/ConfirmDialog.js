38278e1b02a314d0a8215cbd6519c55d3ec60722
Kamuela Franco <kamuela.franco@gmail.com>
2019-05-30 23:31:13 +0100

    Refactor ConfirmDialog component

--- src/components/ConfirmDialog.js
+++ src/components/ConfirmDialog.js
@@ -1,58 +1,70 @@
-/* eslint-disable */
-// TODO: Remove previous line and work through linting issues at next edit
-
 import * as React from 'react';
+import PropTypes from 'prop-types';
 import { Dialog } from '@reach/dialog';
 
-export default class ConfirmStatusChange extends React.Component {
+export default class ConfirmDialog extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      open: false,
+      isOpen: false,
       callback: null,
     };
+    this.show = this.show.bind(this);
+    this.hide = this.hide.bind(this);
+    this.confirm = this.confirm.bind(this);
   }
 
   show() {
     return callback => (event) => {
       event.preventDefault();
 
+      // TODO: Revisit this pattern
+      // eslint-disable-next-line no-param-reassign
       event = {
         ...event,
         target: { ...event.target, value: event.target.value },
       };
 
       this.setState({
-        open: true,
+        isOpen: true,
         callback: () => callback(event),
       });
     };
   }
 
   hide() {
-    this.setState({ open: false, callback: null });
+    this.setState({ isOpen: false, callback: null });
   }
 
   confirm() {
-    this.state.callback();
+    const { callback } = this.state;
+    callback();
     this.hide();
   }
 
   render() {
+    const { children, title, description } = this.props;
+    const { isOpen } = this.state;
     return (
       <React.Fragment>
-        {this.props.children(this.show)}
+        {children(this.show)}
 
-        {this.state.open && (
+        {isOpen && (
           <Dialog>
-            <h1>{this.props.title}</h1>
-            <p>{this.props.description}</p>
+            <h1>{title}</h1>
+            <p>{description}</p>
 
-            <button onClick={this.hide}>Cancel</button>
-            <button onClick={this.confirm}>OK</button>
+            <button type="button" onClick={this.hide}>Cancel</button>
+            <button type="button" onClick={this.confirm}>OK</button>
           </Dialog>
         )}
       </React.Fragment>
     );
   }
 }
+
+ConfirmDialog.propTypes = {
+  description: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  children: PropTypes.arrayOf(PropTypes.element).isRequired,
+};
