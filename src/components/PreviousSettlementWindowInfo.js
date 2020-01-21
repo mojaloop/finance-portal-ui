baaadc40ff8dc65242343730149431dd3a08f296
Kamuela Franco <kamuela.franco@gmail.com>
2020-01-21 15:58:39 +0000

    Remove unnecessary import

--- src/components/PreviousSettlementWindowInfo.js
+++ src/components/PreviousSettlementWindowInfo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes, { array } from 'prop-types';
+import PropTypes from 'prop-types';
 
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
