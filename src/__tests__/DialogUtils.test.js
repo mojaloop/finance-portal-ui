eeddfade79d0ec6addfb1af6efcc615c092172c6
Kamuela Franco <kamuela.franco@gmail.com>
2020-01-22 14:27:30 +0000

    Fix erroneous use of single export

--- src/__tests__/DialogUtils.test.js
+++ src/__tests__/DialogUtils.test.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import DialogUtils from '../components/DialogUtils';
+import { DialogTitle, DialogContent, DialogActions } from '../components/DialogUtils';
 
-describe('<DialogUtils />', () => {
+describe('<DialogTitle />', () => {
   it('should render without crashing', () => {
-    const rendered = render(<DialogUtils />);
+    const rendered = render(<DialogTitle />);
+    expect(rendered).toBeTruthy();
+  });
+});
+
+describe('<DialogContent />', () => {
+  it('should render without crashing', () => {
+    const rendered = render(<DialogContent />);
+    expect(rendered).toBeTruthy();
+  });
+});
+
+describe('<DialogActions />', () => {
+  it('should render without crashing', () => {
+    const rendered = render(<DialogActions />);
     expect(rendered).toBeTruthy();
   });
 });
