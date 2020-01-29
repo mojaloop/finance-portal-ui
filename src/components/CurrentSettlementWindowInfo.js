d4759fa36c7dc5db8790db1b7f4161232f2f9477
Aaron Reynoza <aaron.reynoza@modusbox.com>
2020-01-29 01:59:14 -0600

    fix transactions getting values duplicated

--- src/components/CurrentSettlementWindowInfo.js
+++ src/components/CurrentSettlementWindowInfo.js
@@ -16,21 +16,46 @@ const styles = (theme) => ({
   },
 });
 
+// TODO: Optimize function so it's human readable.
 function consolidateTransactionsByCurrency(transactions) {
+  // Iterate through all the transactions and sum the ones that have the same currency.
   return transactions.reduce((totalPayments, payment) => {
+    // Total number of transactions with the current currency
+    const totalTransactionsForCurrentCurrency = transactions
+      .filter((transaction) => transaction.currencyId === payment.currencyId).length;
     if (totalPayments.length > 0) {
+      // Get a list of all the currencies in the transactions
       const currencies = totalPayments.map((transaction) => transaction.currencyId);
+      // Check if the currency is repeated. if it is, then sum and
+      // return the total transactions-amount/tranfers for that currency;
+      // else, add the new currency and it's values to the array.
       if (currencies.includes(payment.currencyId)) {
         const currencyIndex = currencies.indexOf(payment.currencyId);
+        // skip sum if it's already being done
+        if (totalPayments[currencyIndex]
+          .transactionCounter === totalTransactionsForCurrentCurrency) {
+          return totalPayments;
+        }
         totalPayments[currencyIndex].numTransactions // eslint-disable-line no-param-reassign
         += payment.numTransactions;
         totalPayments[currencyIndex] // eslint-disable-line no-param-reassign
           .senderAmount = (parseFloat(totalPayments[currencyIndex].senderAmount)
           + parseFloat(payment.senderAmount)).toFixed(4);
+
+        if (totalPayments[currencyIndex].transactionCounter) {
+          totalPayments[currencyIndex] // eslint-disable-line no-param-reassign
+            .transactionCounter += 1;
+        } else {
+          totalPayments[currencyIndex] // eslint-disable-line no-param-reassign
+            .transactionCounter = 1; // eslint-disable-line no-param-reassign
+        }
       } else {
         totalPayments.push(payment);
       }
       return totalPayments;
+    }
+    if (!payment.transactionCounter) {
+      payment.transactionCounter = 1; // eslint-disable-line no-param-reassign
     }
     return totalPayments.concat(payment);
   }, []);
