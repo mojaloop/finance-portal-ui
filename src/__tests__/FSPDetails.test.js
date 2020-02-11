74ad4796da8503e0bc40e31508362904506be1c3
Kamuela Franco <kamuela.franco@gmail.com>
2020-02-11 12:33:24 +0000

    Create FSPDetails component

---
+++ src/__tests__/FSPDetails.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import FSPDetails from '../components/FSPDetails';
+
+describe('<FSPDetails />', () => {
+  it('should render without crashing', () => {
+    const props = {
+      classes: {},
+      fsp: {},
+      fpsNamesById: {},
+      setSnackBarParams: () => {},
+    };
+
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    const rendered = render(<FSPDetails {...props} />);
+    expect(rendered).toBeTruthy();
+  });
+});
