dcb344333b741a3a40fe049d4d019adc3791cd1d
Kamuela Franco <kamuela.franco@gmail.com>
2020-02-13 14:17:10 +0000

    Add initial version of test

--- src/__tests__/ForexRateEndDateOption.test.js
+++ src/__tests__/ForexRateEndDateOption.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { DateTime } from 'luxon';
 import ForexRateEndDateOption from '../components/ForexRateEndDateOption';
 
 describe('<ForexRateEndDateOption />', () => {
@@ -11,5 +12,18 @@ describe('<ForexRateEndDateOption />', () => {
     const { findByText } = render(<ForexRateEndDateOption />);
     const element = await findByText('Commit');
     expect(element).toBeDefined();
+  });
+  it('should display the correct end date given the 1 Day option', async () => {
+    const tomorrow8amUTC = String(DateTime.utc()
+      .plus({ days: 1 })
+      .set({
+        hour: 8,
+        minute: 0,
+        second: 0,
+        millisecond: 0,
+      }));
+    const { findAllByText } = render(<ForexRateEndDateOption />);
+    const endDateForOneDay = await findAllByText('End Date')[0];
+    expect(endDateForOneDay.value).toEqual(tomorrow8amUTC);
   });
 });
