925ed88264286711dee554093fec16914ba92997
Kamuela Franco <kamuela.franco@gmail.com>
2020-02-18 12:50:36 +0000

    Add SnackbarUtils story

---
+++ src/stories/SnackbarUtils.stories.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import SnackbarUtils from '../components/SnackbarUtils';
+
+export default {
+  component: SnackbarUtils,
+  title: 'SnackbarUtils',
+  // Our exports that end in "Data" are not stories.
+  excludeStories: /.*Data$/,
+};
+
+// Props
+const classes = {
+
+};
+const message = {
+  success: 'This is a success message',
+  warning: 'This is a warning message',
+  error: 'This is an error message',
+  info: 'This is an info message',
+};
+
+export const Success = () => (
+  <SnackbarUtils classes={classes} variant="success" message={message.success} />
+);
+
+export const Warning = () => (
+  <SnackbarUtils classes={classes} variant="warning" message={message.warning} />
+);
+
+export const Error = () => (
+  <SnackbarUtils classes={classes} variant="error" message={message.error} />
+);
+
+export const Info = () => (
+  <SnackbarUtils classes={classes} variant="info" message={message.info} />
+);
