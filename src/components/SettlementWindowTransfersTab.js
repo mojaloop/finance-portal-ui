5cd728956a212a569e5cb357ae3b6f4368773d8d
Aaron Reynoza <aaron.reynoza@modusbox.com>
2020-02-27 12:10:30 -0600

    add withStyles

--- src/components/SettlementWindowTransfersTab.js
+++ src/components/SettlementWindowTransfersTab.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
-  Grid, Table, TableBody, TableCell, TableHead, TableRow,
+  Grid, Table, TableBody, TableCell, TableHead, TableRow, withStyles,
 } from '@material-ui/core';
 
 function SettlementWindowTransfersTab(props) {
@@ -47,4 +47,4 @@ SettlementWindowTransfersTab.propTypes = {
   classes: PropTypes.shape({ tableDetails: PropTypes.string }).isRequired,
 };
 
-export default SettlementWindowTransfersTab;
+export default withStyles(SettlementWindowTransfersTab);
