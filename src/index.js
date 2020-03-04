8f4c12e6817259239deca2190f147fd9f900ba78
msk- <mattkingston@gmail.com>
2020-03-04 14:49:03 +0000

    Attempt to retain classnames with jssprovider customisation

--- src/index.js
+++ src/index.js
@@ -5,7 +5,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { JssProvider } from 'react-jss';
+import { createGenerateClassName } from '@material-ui/core/styles';
+
+const generateClassName = createGenerateClassName({
+  dangerouslyUseGlobalCSS: true, // won't minify CSS classnames when true
+  productionPrefix: 'c', // 'jss' by default
+});
+
+ReactDOM.render(
+    <JssProvider generateClassName={generateClassName}>
+        <App />
+    </JssProvider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
