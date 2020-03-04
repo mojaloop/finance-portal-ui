d20b816bd31a38f980eade5c76d7f2dfcc117088
Vassilis Barzokas <vassilis.barzokas@modusbox.com>
2020-03-04 18:12:14 +0200

    Downgraded to `react-jss@8.6.1` in order to get the desired functionality in `JssProvider` component.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
 import 'core-js/features/array/flat';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { JssProvider } from 'react-jss';
+import { createGenerateClassName } from '@material-ui/core/styles';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import { JssProvider } from 'react-jss';
-import { createGenerateClassName } from '@material-ui/core/styles';
-
 const generateClassName = createGenerateClassName({
   dangerouslyUseGlobalCSS: true, // won't minify CSS classnames when true
-  productionPrefix: 'c', // 'jss' by default
+  productionPrefix: 'mow', // 'jss' by default
 });
 
 ReactDOM.render(
-    <JssProvider generateClassName={generateClassName}>
-        <App />
-    </JssProvider>,
-    document.getElementById('root')
+  <JssProvider generateClassName={generateClassName}>
+    <App />
+  </JssProvider>,
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
